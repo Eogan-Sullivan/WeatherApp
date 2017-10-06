207d66751054db464d1477d5ff2a2f7179dc8b2b
Eogan Sullivan <eogan.sullivan@outlook.com>
2017-10-06 13:55:54 +0100

    Inital Commit
    First Commit of project

--- WeatherApp/MainActivity.cs
+++ WeatherApp/MainActivity.cs
@@ -7,9 +7,12 @@ namespace WeatherApp
     [Activity(Label = "WeatherApp", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+           
+            
 
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
--- WeatherApp/Resources/Resource.Designer.cs
+++ WeatherApp/Resources/Resource.Designer.cs
@@ -57,6 +57,25 @@ namespace WeatherApp
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int welcomeText = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
